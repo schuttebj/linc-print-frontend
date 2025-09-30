e62473e9e17f2fbeaeda71a4c9995156126e4016
Ben Schutte <schutte.bj@gmail.com>
2025-09-30 18:06:34 +0200

    refactor: update user type checks for admin location selection
    - Modified the conditional rendering logic for admin users in the LearnerPermitCaptureFormPage to check for user type instead of relying solely on primary location ID.
    - Improved clarity and functionality by ensuring that the user type is accurately considered in the location selection process.

--- src/pages/applications/LearnerPermitCaptureFormPage.tsx
+++ src/pages/applications/LearnerPermitCaptureFormPage.tsx
@@ -379,7 +379,7 @@ const LearnerPermitCaptureFormPage: React.FC = () => {
         return (
           <Box>
             {/* Location Selection for Admin Users */}
-            {user && !user.primary_location_id && (
+            {user && user.user_type !== 'LOCATION_USER' && !user.primary_location && (
               <Card 
                 elevation={0}
                 sx={{ 
